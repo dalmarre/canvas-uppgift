c108e64d8a9be5f74d8a9043a899061a75f8e3a9
dalmarre <markus.dalenbring@skola.taby.se>
2022-04-01 11:09:41 +0200

    finished

--- script.js
+++ script.js
@@ -14,12 +14,32 @@ const midY = canvas.height / 2;
 
 // Matar ut canvas dimensioner till konsolen, pröva att
 // ändra storlekn på fönstret och ladda däörefter om sidan.
-console.log(
-  `Bredd på canvas: ${canvas.width},
-Höjd på canvas: ${canvas.height}`
-);
+console.log(`Bredd på canvas: ${canvas.width}, Höjd på canvas: ${canvas.height}`);
 
 function drawPicture() {
-  // Här skriver du funktionen som ritar bilden
-}
+  const height = 1.1*midY
+  const width = midX*0.5
+  c.fillStyle = "#008000"
+  c.strokeRect(canvas.width*0.1, canvas.height-height, width, height+1)
+  c.strokeRect(canvas.width*0.9-width, canvas.height-height, width, height+1)
+  c.beginPath();
+  c.moveTo(canvas.width*0.1, canvas.height-height);
+  c.lineTo(canvas.width*0.1+(width/2), canvas.height-height-(midY*0.4));
+  c.lineTo(canvas.width*0.1+width, canvas.height-height);
+  c.moveTo(canvas.width*0.9-width, canvas.height-height);
+  c.lineTo(canvas.width*0.9-width+(width/2), canvas.height-height-(midY*0.4));
+  c.lineTo(canvas.width*0.9, canvas.height-height); 
+  c.stroke();
+  c.beginPath();
+  c.moveTo(midX-canvas.width*0.075*0.53, midY*1.424);
+  c.ellipse(midX, midY, canvas.width*0.075, midY/2, Math.PI, -1, Math.PI+1);
+  c.fill();
+  c.stroke();
+  c.fillStyle = "#a52a2a"
+  c.beginPath();
+  c.moveTo(midX-canvas.width*0.075*0.53, midY*1.424);
+  c.fillRect(midX-canvas.width*0.075*0.53, midY*1.424, width*0.32, canvas.height-midY*1.424+1);
+  c.strokeRect(midX-canvas.width*0.075*0.53, midY*1.424, width*0.32, canvas.height-midY*1.424+1);
+};
+
 drawPicture();
